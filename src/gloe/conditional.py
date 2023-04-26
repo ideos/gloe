f7f5a2aec03a3fce1f28824e25642798f13bf603
samirbraga <samirchavess@gmail.com>
2023-04-26 10:50:28 -0300

    fix: bug fixes on version 0.2.2

--- src/gloe/conditional.py
+++ src/gloe/conditional.py
@@ -35,10 +35,9 @@ class ConditionerTransformer(Generic[A, B, C], Transformer[A, Union[B, C]]):
     @property
     def signature(self) -> Signature:
         then_signature: Signature = self.then_transformer.signature
-        else_signature: Signature = self.else_transformer.signature
 
         new_signature = then_signature.replace(
-            return_annotation=f"""({then_signature.return_annotation} | {else_signature.return_annotation})"""
+            return_annotation=f"""({self.then_transformer.output_annotation} | {self.else_transformer.output_annotation})"""
         )
         return new_signature
 

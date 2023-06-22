2b41c9d5dfcef6f1d0166ff15f8134fa2a16d75d
samirchaves <samirbraga@live.com>
2023-06-22 13:23:15 -0300

    test: testing Map and MapOver transformers

--- tests/test_transformer_graph.py
+++ tests/test_transformer_graph.py
@@ -3,20 +3,24 @@ import unittest
 from networkx import DiGraph
 
 from lib.conditioners import if_is_even
-from lib.transformers import divide_by_2, minus1, \
+from lib.transformers import divide_by_2, logarithm, minus1, \
     mul_tuple2, natural_logarithm, \
     square, \
     square_root, \
     sum1, \
     sum_tuple2, \
     sum_tuple3, times2
-from src.gloe import Begin
+from src.gloe import Begin, Transformer
 from src.gloe.utils import forward
 
 
 class TestTransformerGraph(unittest.TestCase):
-    def _assert_graph_has_edges(self, transformer, graph, expected_edges):
-
+    def _assert_graph_has_edges(
+        self,
+        transformer: Transformer,
+        graph: DiGraph,
+        expected_edges: list[tuple[str, str]]
+    ):
         ids_by_name = {
             node.__class__.__name__: str(id)
             for id, node in transformer.graph_nodes.items()
@@ -29,14 +33,20 @@ class TestTransformerGraph(unittest.TestCase):
         for edge in edges_with_id:
             self.assertIn(edge, edges)
 
+    def _assert_nodes_count(self, expected: int, graph: DiGraph):
+        nodes = graph.nodes.items()
+        self.assertEqual(expected, len(nodes))
+
+    def _assert_edges_count(self, expected: int, graph: DiGraph):
+        edges = [edge for edge, props in list(graph.edges.items())]
+        self.assertEqual(expected, len(edges))
+
     def test_simplest_case(self):
         identity = square >> square_root >> sum1 >> minus1
         graph: DiGraph = identity.graph()
-        nodes = graph.nodes.items()
-        self.assertEqual(4, len(nodes))
 
-        edges = [edge for edge, props in list(graph.edges.items())]
-        self.assertEqual(3, len(edges))
+        self._assert_nodes_count(4, graph)
+        self._assert_edges_count(3, graph)
 
         expected_edges = [
             ('square', 'square_root'),
@@ -54,15 +64,14 @@ class TestTransformerGraph(unittest.TestCase):
 
         graph: DiGraph = divergent.graph()
 
-        nodes = graph.nodes.items()
-        self.assertEqual(5, len(nodes))  # Each divergent connection has a hidden node
+        self._assert_nodes_count(5, graph)
 
         edges = [edge for edge, props in list(graph.edges.items())]
         # Five edges:
         #           +---->  sum1  ----+
         #  square --+                 +--> (Converge) --> sum_tuple2
         #           +----> minus1 ----+
-        self.assertEqual(5, len(edges))
+        self._assert_edges_count(5, graph)
 
         expected_edges = [
             ('square', 'sum1'),
@@ -83,11 +92,8 @@ class TestTransformerGraph(unittest.TestCase):
 
         graph: DiGraph = divergent.graph()
 
-        nodes = graph.nodes.items()
-        self.assertEqual(11, len(nodes))  # Each divergent connection has a hidden node
-
-        edges = [edge for edge, props in list(graph.edges.items())]
-        self.assertEqual(13, len(edges))
+        self._assert_nodes_count(11, graph)  # Each divergent connection has a hidden node
+        self._assert_edges_count(13, graph)
 
         expected_edges = [
             ('square', 'sum1'),
@@ -118,11 +124,8 @@ class TestTransformerGraph(unittest.TestCase):
 
         graph: DiGraph = divergent.graph()
 
-        nodes = graph.nodes.items()
-        self.assertEqual(10, len(nodes))  # Each divergent connection has a hidden node
-
-        edges = [edge for edge, props in list(graph.edges.items())]
-        self.assertEqual(11, len(edges))
+        self._assert_nodes_count(10, graph)  # Each divergent connection has a hidden node
+        self._assert_edges_count(11, graph)
 
         expected_edges = [
             ('square', 'square_root'),
@@ -141,11 +144,8 @@ class TestTransformerGraph(unittest.TestCase):
 
         graph: DiGraph = conditional.graph()
 
-        nodes = graph.nodes.items()
-        self.assertEqual(5, len(nodes))
-
-        edges = [edge for edge, props in list(graph.edges.items())]
-        self.assertEqual(5, len(edges))
+        self._assert_nodes_count(5, graph)
+        self._assert_edges_count(5, graph)
 
         expected_edges = [
             ('square_root', 'if_is_even'),
@@ -173,11 +173,8 @@ class TestTransformerGraph(unittest.TestCase):
 
         graph: DiGraph = conditional.graph()
 
-        nodes = graph.nodes.items()
-        self.assertEqual(10, len(nodes))
-
-        edges = [edge for edge, props in list(graph.edges.items())]
-        self.assertEqual(11, len(edges))
+        self._assert_nodes_count(10, graph)
+        self._assert_edges_count(11, graph)
 
         expected_edges = [
             ('square_root', 'if_is_even'),
@@ -204,11 +201,8 @@ class TestTransformerGraph(unittest.TestCase):
 
         graph: DiGraph = repeated.graph()
 
-        nodes = graph.nodes.items()
-        self.assertEqual(9, len(nodes))
-
-        edges = [edge for edge, props in list(graph.edges.items())]
-        self.assertEqual(9, len(edges))
+        self._assert_nodes_count(9, graph)
+        self._assert_edges_count(9, graph)
 
     def test_begin_node_case(self):
 
@@ -219,11 +213,8 @@ class TestTransformerGraph(unittest.TestCase):
 
         graph: DiGraph = begin.graph()
 
-        nodes = graph.nodes.items()
-        self.assertEqual(5, len(nodes))
-
-        edges = [edge for edge, props in list(graph.edges.items())]
-        self.assertEqual(5, len(edges))
+        self._assert_nodes_count(5, graph)
+        self._assert_edges_count(5, graph)
 
         expected_edges = [
             ('Begin', 'sum1'),
@@ -251,8 +242,19 @@ class TestTransformerGraph(unittest.TestCase):
 
         graph: DiGraph = begin.graph()
 
-        nodes = graph.nodes.items()
-        self.assertEqual(9, len(nodes))
+        self._assert_nodes_count(9, graph)
+        self._assert_edges_count(10, graph)
 
-        edges = [edge for edge, props in list(graph.edges.items())]
-        self.assertEqual(10, len(edges))
+    def test_transformer_init_case(self):
+        init_graph = logarithm(2) >> square
+
+        graph: DiGraph = init_graph.graph()
+
+        self._assert_nodes_count(2, graph)
+        self._assert_edges_count(1, graph)
+
+        expected_edges = [
+            ('logarithm', 'square'),
+        ]
+
+        self._assert_graph_has_edges(init_graph, graph, expected_edges)

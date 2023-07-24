5093e9c49fb944be4befa7c273a89a23f7b1b2c2
samirchaves <samirbraga@live.com>
2023-07-24 13:28:10 -0300

    fix: fixing bad access to src package

--- src/gloe/experimental/bridge.py
+++ src/gloe/experimental/bridge.py
@@ -1,7 +1,7 @@
 from contextvars import ContextVar
 from typing import Generic, TypeVar
 
-from .gloe.transformers import Transformer
+from ..transformers import Transformer
 
 T = TypeVar("T")
 B = TypeVar("B")

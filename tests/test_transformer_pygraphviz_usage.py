b9ad8e31a5c4a35e62e67f9d29fbf43c2ac6e4e6
castelojb <joaocb14@gmail.com>
2024-04-19 08:31:43 -0300

    chore: add export successful test

--- tests/test_transformer_pygraphviz_usage.py
+++ tests/test_transformer_pygraphviz_usage.py
@@ -1,10 +1,11 @@
+import os.path
 import unittest
 from unittest.mock import patch
 
 from tests.lib.transformers import plus1, square_root
 
 
-class TestPygraphvizImportError(unittest.TestCase):
+class TestPygraphvizUsage(unittest.TestCase):
 
     @patch("builtins.__import__", side_effect=ImportError)
     def test_import_error(self, mock_import):
@@ -12,6 +13,18 @@ class TestPygraphvizImportError(unittest.TestCase):
         foo = square_root >> plus1
 
         self.assertRaises(ImportError, lambda: foo.export("foo.dot"))
+
+    def test_export(self):
+
+        foo = square_root >> plus1
+
+        filepath = "./foo.dot"
+
+        foo.export(filepath)
+
+        self.assertTrue(os.path.exists(filepath))
+
+        os.remove(filepath)
 
 
 if __name__ == "__main__":

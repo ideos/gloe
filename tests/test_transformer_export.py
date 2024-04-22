779d1dc5be8f1068575ea51df7f1dcd0d83d248e
castelojb <joaocb14@gmail.com>
2024-04-22 17:16:16 -0300

    fix: remove successful case of export function

--- tests/test_transformer_export.py
+++ tests/test_transformer_export.py
@@ -1,4 +1,3 @@
-import os.path
 import unittest
 from unittest.mock import MagicMock, patch
 
@@ -13,18 +12,6 @@ class TestTransformerExport(unittest.TestCase):
         foo = square_root >> plus1
 
         self.assertRaises(ImportError, lambda: foo.export("foo.dot"))
-
-    def test_export(self):
-
-        foo = square_root >> plus1
-
-        filepath = "./foo.dot"
-
-        foo.export(filepath)
-
-        self.assertTrue(os.path.exists(filepath))
-
-        os.remove(filepath)
 
 
 if __name__ == "__main__":

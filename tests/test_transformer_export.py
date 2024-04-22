101fbf3886ebe8c8e4b4a860825560797594f413
castelojb <joaocb14@gmail.com>
2024-04-22 17:34:00 -0300

    fix: make foo dag outside test function

--- tests/test_transformer_export.py
+++ tests/test_transformer_export.py
@@ -6,12 +6,12 @@ from tests.lib.transformers import plus1, square_root
 
 class TestTransformerExport(unittest.TestCase):
 
+    foo = square_root >> plus1
+
     @patch("builtins.__import__", side_effect=ImportError)
     def test_import_error(self, mock_import: MagicMock):
 
-        foo = square_root >> plus1
-
-        self.assertRaises(ImportError, lambda: foo.export("foo.dot"))
+        self.assertRaises(ImportError, lambda: self.foo.export("foo.dot"))
 
 
 if __name__ == "__main__":

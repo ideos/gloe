797a453069964eb7e7ece29ccd924257ffcbfdba
Samir Braga <samirchavess@gmail.com>
2024-06-14 10:48:18 -0300

    refactor: exporting new async transformer for collections

--- gloe/collection/__init__.py
+++ gloe/collection/__init__.py
@@ -1,5 +1,8 @@
-__all__ = ["MapOver", "Map", "Filter"]
+__all__ = ["MapOver", "Map", "Filter", "MapOverAsync", "MapAsync", "FilterAsync"]
 
 from gloe.collection._mapover import MapOver
 from gloe.collection._map import Map
 from gloe.collection._filter import Filter
+from gloe.collection._mapover_async import MapOverAsync
+from gloe.collection._map_async import MapAsync
+from gloe.collection._filter_async import FilterAsync

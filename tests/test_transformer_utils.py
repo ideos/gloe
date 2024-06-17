534f0e78b1175919109de42f2187e4d02217cf89
Samir Braga <samirchavess@gmail.com>
2024-06-17 13:25:22 -0300

    tests: adding tests for 'attach' utils

--- tests/test_transformer_utils.py
+++ tests/test_transformer_utils.py
@@ -3,13 +3,18 @@ from typing import Union
 
 from gloe import transformer
 from gloe.transformers import _execute_flow
-from gloe.utils import forward, forward_incoming, debug
+from gloe.utils import forward, forward_incoming, debug, attach
 from tests.lib.transformers import sum_tuple2, square_root, square
 
 
 class TestTransformerUtils(unittest.TestCase):
     def test_forward_incoming(self):
         test_forward = forward_incoming(sum_tuple2)
+
+        self.assertEqual(test_forward((3, 3)), (6, (3, 3)))
+
+    def test_attach(self):
+        test_forward = attach(sum_tuple2)
 
         self.assertEqual(test_forward((3, 3)), (6, (3, 3)))
 
